ee7c6b849cf290d217cb35dbda553190e1ac88b2
queive <queive959@gmail.com>
2026-02-19 19:24:06 -0300

    Aulas de IF

---
+++ Parte 2/1 Nivel Iniciante/Primeiro/Estrutura de decisao.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int numero1, numero2;
+    numero1 = 7, numero2 = 5;
+    if(numero1 > numero2){
+        printf("Numero1 é maior que o Numero2");
+    }
+
+
+
+
+
+    return 0;
+}
