2de751a92a50f373fb824396b380845c6814f2ef
queive <152937464+queive@users.noreply.github.com>
2026-02-20 23:50:09 +0000

    treinando pelo codespace

--- Parte 2/1 Nivel Iniciante/Primeiro/Estrutura de decisao.c
+++ Parte 2/1 Nivel Iniciante/Primeiro/Estrutura de decisao.c
@@ -4,7 +4,7 @@ int main(){
     int numero1, numero2;
     numero1 = 7, numero2 = 5;
     if(numero1 > numero2){
-        printf("Numero1 é maior que o Numero2");
+        printf("Numero1 é maior que o Numero2\n");
     }
 
 
